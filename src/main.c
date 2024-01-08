89e7fd6f40ba8c4a7bcd9fadf59e74faf32f16da
mehoweck <mehoweck@gmail.com>
2024-01-09 00:09:29 +0100

    first working prototype of receiver - without executive code

--- src/main.c
+++ src/main.c
@@ -6,24 +6,28 @@
 
 #define F_CPU 8000000UL
 
+#define INPUT_PIN PB0
+
 #define ADDRESS_LENGTH 4
 #define COMMAND_LENGTH 3
 #define PARAM1_LENGTH 3
 #define PARAM2_LENGTH 3
 #define PARAM3_LENGTH 3
-//#define FRAME_LENGTH 16
+#define FRAME_LENGTH 16
+
 typedef union {
   struct {
-    unsigned int  address : ADDRESS_LENGTH;  // 4 bits for address
-    unsigned int  command : COMMAND_LENGTH;  // 3 bits for command
-    unsigned int  param1 : PARAM1_LENGTH;   // 3 bits for param1
-    unsigned int  param2 : PARAM2_LENGTH;   // 3 bits for param2
-    unsigned int  param3 : PARAM3_LENGTH;   // 3 bits for param3
+    volatile uint8_t address : ADDRESS_LENGTH;  // 4 bits for address
+    volatile uint8_t command : COMMAND_LENGTH;  // 3 bits for command
+    volatile uint8_t param1 : PARAM1_LENGTH;   // 3 bits for param1
+    volatile uint8_t param2 : PARAM2_LENGTH;   // 3 bits for param2
+    volatile uint8_t param3 : PARAM3_LENGTH;   // 3 bits for param3
   } fields;
-  uint8_t rawBytes[2];
+  volatile uint16_t rawPayload;
 } CommandData;
 
 volatile uint8_t ledState = 1;
+volatile CommandData data;
 
 void setup_clock() {
     // CCP = 0xD8;
@@ -34,8 +38,8 @@ void setup_clock() {
 }
 
 void setup_inout() {
-    PIN_OUT(PB1);           // set PB1 as output
-    PIN_IN(PB0);            // set PB0 as input
+    SET_PIN_OUT(PB1);           // set PB1 as output
+    SET_PIN_IN(PB0);            // set PB0 as input
     SET_BIT(PUEB, PUEB0);   // pull-up PB0
 
     SET_BIT(PCICR, PCIE0);  // pin change interrupt control - enable interupt on pin change
@@ -45,44 +49,100 @@ void setup_inout() {
 }
 
 ISR(PCINT0_vect) {
-    PIN_ON(PB2);
-    cli();
-    if(!(PINB & (1 << PB0))) {  // if PB0 is low
-        ledState = !ledState;
+    if(!(PINB & (1 << INPUT_PIN))) {  // if PB0 is low
+        PIN_OFF(PB2);
+        //_delay_us(50);
+        for(uint8_t i = 0; i < FRAME_LENGTH; i++) {
+            _delay_us(99);
+            uint8_t pinValue = (PINB & (1 << INPUT_PIN));
+            data.rawPayload = (data.rawPayload << 1) | pinValue;
 
-        // Write new LED state to PB1
-        if(ledState) {
-            PIN_ON(PB1); // Turn on LED
-        } else {
-            PORTB &= ~(1 << PB1); // Turn off LED
+            if (pinValue) {
+                PIN_OFF(PB2);
+                PIN_ON(PB2);
+            } else {
+                PIN_ON(PB2);
+                PIN_OFF(PB2);
+            }
+        }      
+        PIN_ON(PB2);
+        // uint8_t value = 0;
+        // for(uint8_t i = 0; i < 8; i++) {
+        //     _delay_us(50);
+        //     uint8_t pinValue = (PINB & (1 << INPUT_PIN));
+        //     if (pinValue) {
+        //         PIN_OFF(PB2);
+        //         PIN_ON(PB2);
+        //     } else {
+        //         PIN_ON(PB2);
+        //         PIN_OFF(PB2);
+        //     }
+        //     value = (value << 1) | pinValue;
+        //     _delay_us(50);
+        // }
+
+        _delay_ms(500);
+        PIN_OFF(PB2);
+
+        uint16_t value = data.rawPayload;
+        uint16_t mask = 0x8000; // start with the most significant bit
+        while(mask > 0) {
+            // check if the current bit is 1 or 0
+            if (value & mask) {
+                do_sec_blink(PB1, 500);
+            } else {
+                do_sec_blink(PB1, 100);
+            }
+            mask >>= 1;
         }
+
+        cli();
+
+        PIN_ON(PB2);
+        _delay_ms(200);
+        PIN_OFF(PB2);
+        _delay_ms(200);
+        PIN_ON(PB2);
+        _delay_ms(200);
+        PIN_OFF(PB2);
+        _delay_ms(200);
     }
+    EIFR = 0x01; //clear interrupt
 }
 
 int do_handle_message() {
-    _delay_ms(150);
+    _delay_ms(50);
     sei();  
-    PIN_OFF(PB2); 
+    //do_blink(PB1, 300);
+    //PIN_OFF(PB2); 
     return 0; 
 }
 
 void setup_blink() {
-    PIN_OUT(PB2);
+    SET_PIN_OUT(PB2);
 }
 
-void do_blink(uint8_t pin, uint8_t delay_ms) {
+inline static void do_blink(uint8_t pin, uint16_t delay_ms) {
     PIN_ON(pin);
-    _delay_ms(500);
+    _delay_ms(delay_ms);
     PIN_OFF(pin);
-    _delay_ms(500);
+    _delay_ms(delay_ms);
+}
+
+inline void do_sec_blink(uint8_t pin, uint16_t delay_ms) {
+    PIN_ON(pin);
+    _delay_ms(delay_ms);
+    PIN_OFF(pin);
+    _delay_ms(1000-delay_ms);
 }
 
 int main() {
     setup_clock();
     setup_inout();
-
     setup_blink();
+
     while (1) {
+        PIN_ON(PB2);
         do_handle_message();
         //do_blink(PB2, 200);
     }
--- src/main.h
+++ src/main.h
@@ -6,11 +6,14 @@
 #define SET_BIT(REG, BIT) REG |= 1 << BIT
 #define CLR_BIT(REG, BIT) REG &= ~(1 << BIT)
 
-#define PIN_IN(PIN) CLR_BIT(DDRB, PIN)
-#define PIN_OUT(PIN) SET_BIT(DDRB, PIN)
+#define SET_PIN_IN(PIN) CLR_BIT(DDRB, PIN)
+#define SET_PIN_OUT(PIN) SET_BIT(DDRB, PIN)
 
 #define PIN_ON(PIN) PORTB |= 1 << PIN
 #define PIN_OFF(PIN) PORTB &= ~(1 << PIN)
+
+#define PIN_IS_LO(PIN) !(PINB & (1 << PIN))
+#define PIN_IS_HI(PIN) PINB & (1 << PIN)
 
 //#define PIN_ON(PORT, PIN) PORT |= 1 << PIN
 //#define PIN_OFF(PORT, PIN) PORT &= ~(1 << PIN)
